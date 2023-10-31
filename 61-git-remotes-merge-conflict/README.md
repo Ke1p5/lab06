5a6d05b726bfa820577dd9b584ab1a56f376df84
Federico Capponi <federico.capponi@studio.unibo.it>
2023-10-31 12:58:17 +0100

    Finished Exercises 61 and 62

--- 61-git-remotes-merge-conflict/README.md
+++ 61-git-remotes-merge-conflict/README.md
@@ -11,16 +11,56 @@ Prima di eseguire ogni operazione sul worktree o sul repository,
 si verifichi lo stato del repository con `git status`.
 
 1. Si cloni localmente il repository
+
+git clone https://github.com/APICe-at-DISI/OOP-git-merge-conflict-test
+Cloning into 'OOP-git-merge-conflict-test'...
+remote: Enumerating objects: 12, done.
+remote: Counting objects: 100% (4/4), done.
+remote: Compressing objects: 100% (3/3), done.
+remote: Total 12 (delta 1), reused 1 (delta 1), pack-reused 8
+Receiving objects: 100% (12/12), done.
+Resolving deltas: 100% (2/2), done.
+
 2. Ci si assicuri di avere localmente entrambi i branch remoti
+
+git log --oneline --all --graph
+* bed943f (origin/feature) Print author information
+| * 8e0f29c (HEAD -> master, origin/master, origin/HEAD) Change HelloWorld to print the number of available processors
+|/  
+* d956df6 Create .gitignore
+* 700ee0b Create HelloWorld
+
 3. Si faccia il merge di `feature` dentro `master`, ossia: si posizioni la `HEAD` su `master`
    e da qui si esegua il merge di `feature`
+
+git merge origin/feature
+Auto-merging HelloWorld.java
+CONFLICT (content): Merge conflict in HelloWorld.java
+Automatic merge failed; fix conflicts and then commit the result.
+
+
 4. Si noti che viene generato un **merge conflict**!
 5. Si risolva il merge conflict come segue:
    - Il programma Java risultante deve stampare sia il numero di processori disponibili
      (funzionalità presente su `master`)
      che il nome dell'autore del file
      (funzionalità presente su `feature`)
+     
+git commit -m 'Fixed unreachable code, accepted both changes and updated the author'
+git merge origin/feature
+
 6. Si crei un nuovo repository nel proprio github personale
 7. Si aggiunga il nuovo repository creato come **remote** e si elenchino i remote
+
+git remote add origin https://github.com/Ke1p5/git-merge-conflict-test
+git remote show origin
+
+
 8. Si faccia push del branch `master` sul proprio repository
+
+git branch -M master
+git push -u origin master
+
 9. Si setti il branch remoto `master` del nuovo repository come *upstream* per il proprio branch `master` locale
+
+git branch --set-upstream-to=origin/master
--- 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
+++ 62-use-lists-and-maps/src/main/java/it/unibo/collections/UseListsAndMaps.java
@@ -1,7 +1,11 @@
 package it.unibo.collections;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Example class using {@link List} and {@link Map}.
@@ -9,31 +13,80 @@ import java.util.Map;
  */
 public final class UseListsAndMaps {
 
-    private UseListsAndMaps() {
+    private static final int LOW = 1_000;
+    private static final int HIGH = 2_000;
+    private static final int ELEMS = 100_000;
+
+
+
+    private static void printTime(final List<Integer> list, final String op, Long time, int number) {
+        
+        time = System.nanoTime() - time;
+        final var millis = TimeUnit.NANOSECONDS.toMillis(time);
+        System.out.println(// NOPMD
+            op + " "
+                + number
+                + " ints in an ArrayList took "
+                + time
+                + "ns ("
+                + millis
+                + "ms)"
+        );
     }
 
     /**
      * @param s
      *            unused
      */
-    public static void main(final String... s) {
+    public static void main(final String[] s) {
         /*
          * 1) Create a new ArrayList<Integer>, and populate it with the numbers
          * from 1000 (included) to 2000 (excluded).
          */
+
+        List<Integer> arr = new ArrayList<>(1000);
+
+        for (int i = LOW; i < HIGH; i++) {
+            arr.add(i);
+        }
+
+
         /*
          * 2) Create a new LinkedList<Integer> and, in a single line of code
          * without using any looping construct (for, while), populate it with
          * the same contents of the list of point 1.
          */
+
+        List<Integer> link = new LinkedList<>(arr);
+
         /*
          * 3) Using "set" and "get" and "size" methods, swap the first and last
          * element of the first list. You can not use any "magic number".
          * (Suggestion: use a temporary variable)
          */
+
+        final int pHolder = arr.get(0);
+        arr.set(0, arr.get(arr.size() - 1));
+        arr.set(arr.size() - 1, pHolder);
+
         /*
          * 4) Using a single for-each, print the contents of the arraylist.
          */
+
+        System.out.print("Arr: ");
+        for (Integer elem: arr) {
+            System.out.print(elem + " ");
+        }
+
+        System.out.println();
+
+        System.out.print("Link: ");
+        for (Integer elem: link) {
+            System.out.print(elem + " ");
+        }
+
+        System.out.println();
+
         /*
          * 5) Measure the performance of inserting new elements in the head of
          * the collection: measure the time required to add 100.000 elements as
@@ -41,12 +94,97 @@ public final class UseListsAndMaps {
          * using the previous lists. In order to measure times, use as example
          * TestPerformance.java.
          */
+
+        long timeArr = System.nanoTime();
+        for (int i = 1; i <= ELEMS; i++) {
+            arr.add(0, i);
+        }
+
+        printTime(arr, "Adding", timeArr, ELEMS);
+
+        /*
+        timeArr = System.nanoTime() - timeArr;
+        var millisArr = TimeUnit.NANOSECONDS.toMillis(timeArr);
+        System.out.println(// NOPMD
+            "Adding "
+                + arr.size()
+                + " ints in an ArrayList took "
+                + timeArr
+                + "ns ("
+                + millisArr
+                + "ms)"
+        );
+        */
+        
+        long timeLink = System.nanoTime();
+        for (int i = 1; i <= ELEMS; i++) {
+            link.add(0, i);
+        }
+
+        printTime(link, "Adding", timeLink, ELEMS);
+
+        /*
+        timeLink = System.nanoTime() - timeLink;
+        var millisLink = TimeUnit.NANOSECONDS.toMillis(timeLink);
+        System.out.println(// NOPMD
+            "Adding "
+                + ELEMS
+                + " ints in a LinkedList took "
+                + timeLink
+                + "ns ("
+                + millisLink
+                + "ms)"
+        ); */
+
         /*
          * 6) Measure the performance of reading 1000 times an element whose
          * position is in the middle of the collection for both ArrayList and
          * LinkedList, using the collections of point 5. In order to measure
          * times, use as example TestPerformance.java.
          */
+        
+        timeArr = System.nanoTime();
+        for (int i = 1; i <= LOW; i++) {
+            arr.get(arr.size()/2);
+        }
+
+        printTime(arr, "Reading", timeArr, LOW);
+        
+        /*
+        timeArr = System.nanoTime() - timeArr;
+        millisArr = TimeUnit.NANOSECONDS.toMillis(timeArr);
+        System.out.println(// NOPMD
+            "Reading "
+                + LOW
+                + " ints in a ArrayList took "
+                + timeArr
+                + "ns ("
+                + millisArr
+                + "ms)"
+        );
+        */
+
+        timeLink = System.nanoTime();
+        for (int i = 1; i <= LOW; i++) {
+            link.get(link.size()/2);
+        }
+        
+        printTime(link, "Reading", timeLink, LOW);
+
+        /*
+        timeLink = System.nanoTime() - timeLink;
+        millisLink = TimeUnit.NANOSECONDS.toMillis(timeLink);
+        System.out.println(// NOPMD
+            "Reading "
+                + LOW
+                + " ints in a LinkedList took "
+                + timeArr
+                + "ns ("
+                + millisLink
+                + "ms)"
+        );
+        */
+
         /*
          * 7) Build a new Map that associates to each continent's name its
          * population:
@@ -63,8 +201,25 @@ public final class UseListsAndMaps {
          *
          * Oceania -> 38,304,000
          */
+
+        final Map<String, Long> continents = new HashMap<>();
+
+        continents.put("Africa", 1_110_635_000L);
+        continents.put("Americas", 972_005_000L);
+        continents.put("Antarctica", 0L);
+        continents.put("Asia", 4_298_723_000L);
+        continents.put("Europe", 742_452_000L);
+        continents.put("Oceania", 38_304_000L);
+
         /*
          * 8) Compute the population of the world
          */
+
+        long wp = 0;
+        for (Long l: continents.values()) {
+            wp += l;
+        }
+
+        System.out.println("Total World population is: " + wp);
     }
 }
